8c904356687f573e42046d56d2e834344155a05a
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-29 15:39:59 -0300

    Inicializando projeto BigBen

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-violet-700 w-full">
+    <h1 class="font-bold px-4 py-5">This is the project start</h1>
+  </div>
+  <div class="m-4 flex justify-center">
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <UButton 
+    label="Button" 
+    size="md"/>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
