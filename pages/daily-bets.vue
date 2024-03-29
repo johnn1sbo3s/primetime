806a3e81ef6d38784fb6e182003e01b2dad279b2
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-29 16:38:54 -0300

    cria tela apostas do dia

---
+++ pages/daily-bets.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full p-6">
+        <h1 class="text-2xl font-semibold">Apostas do dia</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
