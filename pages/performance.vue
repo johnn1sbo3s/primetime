4a7a20e453aa759e7ff2c917c53b3de786819235
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-30 18:49:28 -0300

    padroniza background das outras views

--- pages/performance.vue
+++ pages/performance.vue
@@ -1,13 +1,11 @@
 <template>
-    <h1>Performance</h1>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    <div class="w-full">
+      <div class="flex justify-center">
+        <p>Tela de performance dos modelos</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style lang="scss" scoped></style>
