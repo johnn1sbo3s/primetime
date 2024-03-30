e44a7e591ca0e3d2657c1750c8dc719dcd3636c9
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-30 16:25:09 -0300

    add lógica da tabela e filtro do select

--- pages/daily-bets.vue
+++ pages/daily-bets.vue
@@ -8,52 +8,94 @@
             />
         </div>
         <div class="my-4">
-            <UTable 
-            :rows="people"
-            :key="people.id" 
+            <div class="text-sm text-slate-400 mb-3"
+            v-if="bets.length > 0"
+            >
+                {{ qtd_games }} apostas encontradas
+            </div>
+            <UTable
+            class="border border-gray-700 rounded-lg"
+            :rows="bets"
+            :columns="columns"
+            :sort="sort"
             />
         </div>
     </div>
 </template>
 
 <script setup>
-const people = [
-        {
-            "ID": 1,
-            "Date": "2024-03-29",
-            "Home": "Le Mans",
-            "Away": "Epinal",
-            "FT_Odds_H": 1.67,
-            "FT_Odds_D": 3.7,
-            "FT_Odds_A": 5.6,
-            "Modelo": "lay_away_v1_betfair"
-        },
-        {
-            "ID": 2,
-            "Date": "2024-03-29",
-            "Home": "QPR",
-            "Away": "Birmingham",
-            "FT_Odds_H": 2.04,
-            "FT_Odds_D": 3.48,
-            "FT_Odds_A": 3.66,
-            "Modelo": "lay_away_v8"
-        },
-        {
-            "ID": 3,
-            "Date": "2024-03-29",
-            "Home": "Eindhoven",
-            "Away": "Cambuur",
-            "FT_Odds_H": 2.57,
-            "FT_Odds_D": 3.65,
-            "FT_Odds_A": 2.46,
-            "Modelo": "over_25_v4"
-        }
+import { ref } from 'vue'
+
+const sort = { column : 'Modelo', direction: 'asc' };
+
+const columns = [
+  { key: 'Date', label: 'Date' },
+  { key: 'Home', label: 'Home Team', sortable: true },
+  { key: 'Away', label: 'Away Team', sortable: true },
+  { key: 'FT_Odds_H', label: 'Odds Home' },
+  { key: 'FT_Odds_D', label: 'Odds Draw' },
+  { key: 'FT_Odds_A', label: 'Odds Away' },
+  { key: 'Modelo', label: 'Modelo', sortable: true },
 ]
 
-const dates = ['2024-03-29', '2024-03-28', '2024-03-27', '2024-03-26']
-const date = ref(dates[0])
+const filterByDate = (data, selectedDate) => {
+    return Object.values(data).filter(item => item.Date === selectedDate);
+};
+
+const normalizeColumns = (object_data) => {
+    object_data.forEach(item => {
+        item.Modelo = item.Modelo.replace(/_/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
+        item.FT_Odds_H = parseFloat(item.FT_Odds_H).toFixed(2);
+        item.FT_Odds_D = parseFloat(item.FT_Odds_D).toFixed(2);
+        item.FT_Odds_A = parseFloat(item.FT_Odds_A).toFixed(2);
+    })
+    
+    return object_data
+}
+
+const fetchData = async () => {
+    try {
+        const req = await fetch('http://localhost:5000/daily_bets');
+        const data = await req.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
+        return [];
+    }
+};
+
+const dates = ref([]);
+
+const games = await fetchData();
+const uniqueDates = new Set();
+Object.values(games).forEach(item => {
+    uniqueDates.add(item.Date);
+});
+
+dates.value = Array.from(uniqueDates);
+const date = ref(dates.value[dates.value.length - 1])
+
+const bets = ref([]);
+
+const buildTableData = async (chosenDate) => {
+  try {
+    const filteredBets = filterByDate(games, chosenDate);
+    normalizeColumns(filteredBets);
+    bets.value = filteredBets;
+  } catch (error) {
+    console.error('Erro ao buscar apostas do dia:', error);
+    bets.value = []; // Limpar a lista em caso de erro
+  }
+};
+
+const qtd_games = computed(() => bets.value.length);
+
+watchEffect(() => {
+    buildTableData(date.value);
+});
 
 </script>
+
 
 <style>
 
