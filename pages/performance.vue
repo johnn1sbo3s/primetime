74fd1398b3472f4d848b99a4cef2362354db9b85
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-04-02 16:50:17 -0300

    wip: cria os cards de informações de val e real

--- pages/performance.vue
+++ pages/performance.vue
@@ -15,7 +15,7 @@
         class="w-1/4"
         placeholder="Selecione um modelo"
         :options="listModels"
-        :model-value="chosenModel"
+        v-model:model-value="chosenModel"
       />
     </div>
     <div class="w-full gap-4 flex">
@@ -24,48 +24,102 @@
           class="mb-3 hover:outline hover:outline-green-300 hover:outline-1"
         >
           <div class="flex flex-col">
-            <p class="font-semibold">Métricas da validação</p>
+            <p class="font-semibold">Métricas de validação</p>
             <div class="font-medium text-sm">
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Profit:</p>
-                <p>ROI:</p>
-                <p>Precisão:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Profit:</p>
+                  <p>{{ valData.plb.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">ROI:</p>
+                  <p>{{ valData.roi.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">WR:</p>
+                  <p>{{ valData.wr.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Odd média:</p>
-                <p>Win médio:</p>
-                <p>Loss médio:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Odd média:</p>
+                  <p>{{ valData.odds.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Win médio:</p>
+                  <p>{{ valData.med_gain.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Loss médio:</p>
+                  <p>{{ valData.med_loss.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>EV:</p>
-                <p>Máx DD:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">EV:</p>
+                  <p>{{ valData.ev.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Máx DD:</p>
+                  <p>{{ valData.dd.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Entradas:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Entradas:</p>
+                  <p>{{ valData.entradas }}</p>
+                </div>
               </div>
             </div>
           </div>
         </UCard>
         <UCard class="hover:outline hover:outline-green-300 hover:outline-1">
           <div class="flex flex-col">
-            <p class="font-semibold">Métricas da validação</p>
+            <p class="font-semibold">Métricas de jogos reais</p>
             <div class="font-medium text-sm">
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Profit:</p>
-                <p>ROI:</p>
-                <p>Precisão:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Profit:</p>
+                  <p>{{ realData.plb.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">ROI:</p>
+                  <p>{{ realData.roi.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">WR:</p>
+                  <p>{{ realData.wr.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Odd média:</p>
-                <p>Win médio:</p>
-                <p>Loss médio:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Odd média:</p>
+                  <p>{{ realData.odds.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Win médio:</p>
+                  <p>{{ realData.med_gain.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Loss médio:</p>
+                  <p>{{ realData.med_loss.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>EV:</p>
-                <p>Máx DD:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">EV:</p>
+                  <p>{{ realData.ev.toFixed(2) }}</p>
+                </div>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Máx DD:</p>
+                  <p>{{ realData.dd.toFixed(2) }}</p>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-2 my-4">
-                <p>Entradas:</p>
+                <div class="flex gap-1">
+                  <p class="font-semibold">Entradas:</p>
+                  <p>{{ realData.entradas }}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -75,14 +129,14 @@
         <template #header>
           <div class="flex justify-between">
             <p class="font-semibold">Gráfico de acúmulo de capital</p>
-            <div class="flex">
+            <div class="flex gap-2">
               <p class="text-sm">Exibir validação</p>
               <UToggle
                 size="md"
                 on-icon="i-heroicons-check-20-solid"
                 off-icon="i-heroicons-x-mark-20-solid"
-                :model-value="favsOnly"
-                @click="changeFavsOnly"
+                :model-value="showVal"
+                @click="changeShowVal"
               />
             </div>
           </div>
@@ -95,12 +149,12 @@
             v-bind="lineChartProps"
           />
         </div>
-        <div class="mt-3 flex justify-between">
-          <p class="text-sm text-slate-300 font-semibold mt-5">Profit:</p>
-          <p class="text-sm text-slate-300 font-semibold mt-5">ROI:</p>
-          <p class="text-sm text-slate-300 font-semibold mt-5">Precisão:</p>
-          <p class="text-sm text-slate-300 font-semibold mt-5">EV:</p>
-          <p class="text-sm text-slate-300 font-semibold mt-5">Entradas:</p>
+        <div class="mt-3 px-2 flex justify-between">
+          <p class="text-sm font-semibold mt-5">Profit:</p>
+          <p class="text-sm font-semibold mt-5">ROI:</p>
+          <p class="text-sm font-semibold mt-5">Precisão:</p>
+          <p class="text-sm font-semibold mt-5">EV:</p>
+          <p class="text-sm font-semibold mt-5">Entradas:</p>
         </div>
       </UCard>
     </div>
@@ -110,8 +164,12 @@
 
 <script setup>
 import { Chart, registerables } from "chart.js";
+import { initCustomFormatter } from "vue";
 import { LineChart } from "vue-chart-3";
 Chart.register(...(registerables || []));
+
+const realData = ref({});
+const valData = ref({});
 
 const fetchData = async () => {
   try {
@@ -124,6 +182,11 @@ const fetchData = async () => {
   }
 };
 
+const showVal = ref(true);
+const changeShowVal = () => {
+  showVal.value = !showVal.value;
+};
+
 const performance_data = await fetchData();
 const listModels = ref([]);
 Object.values(performance_data).forEach((item) => {
@@ -134,9 +197,6 @@ Object.values(performance_data).forEach((item) => {
   }
 });
 
-const chosenModel = ref(listModels.value[0]);
-console.log(listModels);
-
 const chartData = {
   labels: ["Paris", "Nîmes", "Toulon", "Perpignan", "Autre"],
   datasets: [
@@ -145,6 +205,24 @@ const chartData = {
     },
   ],
 };
+
+const objectModel = ref({});
+const chosenModel = ref(listModels.value[0]);
+const changeModel = () => {
+  Object.values(performance_data).forEach((item) => {
+    let name = item.modelo;
+    name = name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
+    if (name === chosenModel.value) {
+      objectModel.value = item;
+      const { real } = objectModel.value;
+      const { val } = objectModel.value;
+      realData.value = real;
+      valData.value = val;
+    }
+  });
+};
+
+changeModel();
 
 const lineChartProps = {
   width: 400,
@@ -162,6 +240,10 @@ const chartOptions = {
     },
   },
 };
+
+watchEffect(() => {
+  changeModel();
+});
 </script>
   
 <style lang="scss" scoped></style>
