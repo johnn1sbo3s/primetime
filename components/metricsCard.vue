14959531b8a720cebdd20136bb6400148adae8e6
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-04-05 14:46:37 -0300

    cria componente de card de métricas

---
+++ components/metricsCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <UCard class="hover:outline hover:outline-green-300 hover:outline-1">
+    <template #header>
+      <p class="font-semibold">{{ cardTitle }}</p>
+    </template>
+    <div class="text-sm">
+      <div class="grid grid-cols-3 gap-2">
+        <div class="flex gap-1">
+          <p class="font-semibold">Profit:</p>
+          <p>{{ metricsData.plb.toFixed(2) }}</p>
+        </div>
+        <div class="flex gap-1">
+          <p class="font-semibold">ROI:</p>
+          <p>{{ metricsData.roi.toFixed(2) }}</p>
+        </div>
+        <div class="flex gap-1">
+          <p class="font-semibold">WR:</p>
+          <p>{{ metricsData.wr.toFixed(2) }}</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 gap-2 my-4">
+        <div class="flex gap-1">
+          <p class="font-semibold">Odd média:</p>
+          <p>{{ metricsData.odds.toFixed(2) }}</p>
+        </div>
+        <div class="flex gap-1">
+          <p class="font-semibold">Win médio:</p>
+          <p>{{ metricsData.med_gain.toFixed(2) }}</p>
+        </div>
+        <div class="flex gap-1">
+          <p class="font-semibold">Loss médio:</p>
+          <p>{{ metricsData.med_loss.toFixed(2) }}</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 gap-2">
+        <div class="flex gap-1">
+          <p class="font-semibold">EV:</p>
+          <p>{{ metricsData.ev.toFixed(2) }}</p>
+        </div>
+        <div class="flex gap-1">
+          <p class="font-semibold">Máx DD:</p>
+          <p>{{ metricsData.dd.toFixed(2) }}</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 gap-2 my-4">
+        <div class="flex gap-1">
+          <p class="font-semibold">Entradas:</p>
+          <p>{{ metricsData.entradas }}</p>
+        </div>
+      </div>
+    </div>
+  </UCard>
+</template>
+
+<script setup>
+const props = defineProps(["metricsData", "cardTitle"]);
+</script>
+
+<style scoped></style>
