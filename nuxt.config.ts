cdeb51350fc6a686ef397b24f7bc22b4cce9d953
john1sbo3s <joaopaulosncastro@gmail.com>
2024-04-07 13:08:46 -0300

    instala lodash

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui',],
+  modules: ['@nuxt/ui', "nuxt-lodash",],
   devtools: { enabled: true },
+
+  lodash: {
+    prefix: "_",
+    prefixSkip: ["string"],
+    upperAfterPrefix: false,
+    exclude: ["map"],
+    alias: [
+      ["camelCase", "stringToCamelCase"], // => stringToCamelCase
+      ["kebabCase", "stringToKebab"], // => stringToKebab
+      ["isDate", "isLodashDate"], // => _isLodashDate
+    ],
+  },
 })
