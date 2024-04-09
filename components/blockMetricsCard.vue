f45b138044a0785688f48aefdabead320b828b1e
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-04-09 13:47:52 -0300

    passa card de métricas avançadas pra um componente separado

---
+++ components/blockMetricsCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <UCard class="h-full">
+    <template #header>
+      <p class="font-semibold">{{ cardTitle }}</p>
+    </template>
+    <div class="grid grid-cols-2 text-sm gap-y-5">
+      <div class="flex gap-1">
+        <p class="font-semibold">Média P/L:</p>
+        <p>{{ metricsData.media.toFixed(2) }}</p>
+      </div>
+      <div class="flex gap-1">
+        <p class="font-semibold">Desvpad:</p>
+        <p>{{ metricsData.desvpad.toFixed(2) }}</p>
+      </div>
+      <div class="flex gap-1">
+        <p class="font-semibold">Méd/Desvpad:</p>
+        <p>{{ metricsData.med_dp.toFixed(2) }}</p>
+      </div>
+      <div class="flex gap-1">
+        <p class="font-semibold">Dif. valid.:</p>
+        <p>{{ metricsData.diff_med_dp_um_96_raiz.toFixed(2) }}</p>
+      </div>
+      <div class="flex gap-1">
+        <p class="font-semibold">Int. conf.:</p>
+        <p>
+          {{ metricsData.intervalo_confianca[0].toFixed(2) }} a
+          {{ metricsData.intervalo_confianca[1].toFixed(2) }}
+        </p>
+      </div>
+    </div>
+  </UCard>
+</template>
+
+<script setup>
+const props = defineProps(["metricsData", "cardTitle"]);
+</script>
--- pages/performance.vue
+++ pages/performance.vue
@@ -71,36 +71,10 @@
         </template>
         <div class="flex h-full gap-3">
           <div class="flex flex-col gap-3 w-2/5">
-            <UCard class="h-full">
-              <template #header>
-                <p class="font-semibold">Médias</p>
-              </template>
-              <div class="grid grid-cols-2 text-sm gap-y-5">
-                <div class="flex gap-1">
-                  <p class="font-semibold">Média P/L:</p>
-                  <p>{{ totalData.media.toFixed(2) }}</p>
-                </div>
-                <div class="flex gap-1">
-                  <p class="font-semibold">Desvpad:</p>
-                  <p>{{ totalData.desvpad.toFixed(2) }}</p>
-                </div>
-                <div class="flex gap-1">
-                  <p class="font-semibold">Méd/Desvpad:</p>
-                  <p>{{ totalData.med_dp.toFixed(2) }}</p>
-                </div>
-                <div class="flex gap-1">
-                  <p class="font-semibold">Dif. valid.:</p>
-                  <p>{{ totalData.diff_med_dp_um_96_raiz.toFixed(2) }}</p>
-                </div>
-                <div class="flex gap-1">
-                  <p class="font-semibold">Int. conf.:</p>
-                  <p>
-                    {{ totalData.intervalo_confianca[0].toFixed(2) }} a
-                    {{ totalData.intervalo_confianca[1].toFixed(2) }}
-                  </p>
-                </div>
-              </div>
-            </UCard>
+            <block-metrics-card
+              :metrics-data="totalData"
+              :card-title="'Médias'"
+            />
             <UCard class="h-full">
               <template #header>
                 <p class="font-semibold">Bloco atual</p>
