127c7a518af5927205616e494e66e08b12fcfcde
john1sbo3s <joaopaulosncastro@gmail.com>
2024-04-11 23:20:17 -0300

    add tela de comparação dos modelos

--- pages/comparison.vue
+++ pages/comparison.vue
@@ -6,15 +6,18 @@
       searchable
       searchable-placeholder="Pesquise por categoria"
       placeholder="Selecione uma categoria"
-      :options="modelsList"
+      :options="categoriesList"
       v-model:model-value="chosenCategory"
     />
     <div class="h-96 w-full">
       <div>
-        <p class="mb-3 text-sm">5 modelos encontrados</p>
+        <p v-if="performanceDataRows.length > 0" class="mb-3 text-sm">
+          {{ performanceDataRows.length }} modelos
+        </p>
         <UTable
           class="border border-gray-700 rounded-lg"
           :rows="performanceDataRows"
+          :columns="performanceDataColumns"
         />
       </div>
     </div>
@@ -22,16 +25,84 @@
 </template>
 
 <script setup>
-const chosenCategory = ref("");
+const chosenCategory = ref("Selecione uma categoria");
 const performanceDataRows = ref([]);
+
+const categoriesList = [
+  "Betfair",
+  "Lay Away",
+  "LTD",
+  "Lay_Home",
+  "Lay Zebra",
+  "Back Home",
+  "Back Away",
+  "Over 25",
+  "Over 05HT",
+  "Back Draw",
+  "Lay Goleada",
+  "Lay 0x2",
+  "Lay 2x0",
+];
+
+const performanceDataColumns = ref([
+  { key: "modelo", label: "Modelo" },
+  { key: "media", label: "Média P/L", sortable: true },
+  { key: "desvpad", label: "Desvpad P/L", sortable: true },
+  { key: "diff_med_dp_um_96_raiz", label: "Dif. valid.", sortable: true },
+  { key: "bottom_int", label: "Int. conf. inferior", sortable: true },
+  { key: "top_int", label: "Int. conf. superior", sortable: true },
+  { key: "med_atual", label: "Média atual" },
+  { key: "desvpad_atual", label: "Desvpad atual" },
+  { key: "n_games", label: "Jogos" },
+]);
+
+const modelNameToBack = (modelName) => {
+  return modelName.toLowerCase().replace(/\s+/g, "_");
+};
+
+const modelNameToFront = (modelName) => {
+  return modelName.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
+};
+
+function buildTableObject(objectList) {
+  let objects = [];
+  objectList.forEach((item) => {
+    let myObj = {
+      modelo: modelNameToFront(item.modelo),
+      media: item.total.media.toFixed(2),
+      desvpad: item.total.desvpad.toFixed(2),
+      med_dp: item.total.med_dp.toFixed(2),
+      diff_med_dp_um_96_raiz: item.total.diff_med_dp_um_96_raiz.toFixed(2),
+      bottom_int: item.total.intervalo_confianca[0].toFixed(2),
+      top_int: item.total.intervalo_confianca[1].toFixed(2),
+      med_atual: item.total.media_atual.toFixed(2),
+      desvpad_atual: item.total.desvpad_atual.toFixed(2),
+      n_games: item.total.qtd_jgs_atual,
+    };
+    objects.push(myObj);
+  });
+  return objects;
+}
 
 const { data: performanceData, error } = await useFetch(
   "http://localhost:5000/model_performance"
 );
 
-const models = ref(Object.values(performanceData.value));
-const modelsList = normalizeModelName(models.value.map((item) => item.modelo));
-chosenCategory.value = modelsList[0];
+const buildComparisonTable = () => {
+  performanceDataRows.value = [];
+
+  let filteredModels = [];
+  _forEach(performanceData.value, function (value, key) {
+    if (value.modelo.includes(modelNameToBack(chosenCategory.value))) {
+      filteredModels.push(value);
+    }
+  });
+  performanceDataRows.value = buildTableObject(filteredModels);
+};
+
+watchEffect(() => {
+  buildComparisonTable();
+});
 </script>
 
 <style>
