d19cebc8418c21bce3e814e822216b90af60c45d
john1sbo3s <joaopaulosncastro@gmail.com>
2024-07-13 11:38:43 -0300

    Corrige título da página

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
 <template>
-  <div class="flex justify-center">
-    <div class="flex flex-col gap-y-3">
-    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-      ISTO NÃO É UMA RECOMENDAÇÃO DE INVESTIMENTO. APOSTE POR SUA CONTA E RISCO!
-    </div>
-      <div class="flex justify-center">
-        <UButton size="lg">
-          <a
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
-            target="_blank"
-            >Preview</a
-          >
-        </UButton>
-      </div>
-    </div>
+  <div class="flex flex-col gap-3">
+    <page-header title="Bem-vindo ao PrimeTime!" />
   </div>
 </template>
 
