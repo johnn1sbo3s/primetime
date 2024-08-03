7ead0fd6fcf8e57b4ccd068160f71797030a78f4
john1sbo3s <joaopaulosncastro@gmail.com>
2024-08-03 18:19:54 -0300

    Passa a responsabilidade das requisições para a página dashboard

--- components/bankrollEvolution.vue
+++ components/bankrollEvolution.vue
@@ -1,49 +1,41 @@
 <template>
-    <div
-        v-if="pending"
-        class="flex gap-4"
-    >
-        <USkeleton class="h-[400px] w-4/6" />
-        <USkeleton class="h-[400px] w-2/6" />
-    </div>
+	<div class="flex gap-3">
+		<div class="w-4/6">
+			<LineChart
+			:chartData="chartData"
+			:options="chartOptions"
+			:style="chartStyle"
+			/>
+		</div>
 
-    <div v-else class="flex gap-3">
-        <div class="w-4/6">
-            <LineChart
-            :chartData="chartData"
-            :options="chartOptions"
-            :style="chartStyle"
-            />
-        </div>
+		<div class="w-2/6 pt-8 pb-7 h-full">
+			<u-card class="h-full">
+				<template #header>
+					<p class="font-semibold">Dados da evolução</p>
+				</template>
 
-        <div class="w-2/6 pt-8 pb-7 h-full">
-            <u-card class="h-full">
-                <template #header>
-                    <p class="font-semibold">Dados da evolução</p>
-                </template>
+				<template #default>
+					<div class="flex flex-col gap-4 max-h-56 overflow-y-scroll">
+						<div
+							v-for="item in resultsByMonth"
+							:key="item.month"
+						>
+							<div class="flex justify-between pr-4">
+								<p>{{ item.month }}</p>
 
-                <template #default>
-                    <div class="flex flex-col gap-4 max-h-56 overflow-y-scroll">
-                        <div
-                            v-for="item in resultsByMonth"
-                            :key="item.month"
-                        >
-                            <div class="flex justify-between pr-4">
-                                <p>{{ item.month }}</p>
-
-                                <p
-                                    class="font-semibold"
-                                    :class="item.profit >= 0 ? 'text-purple-600' : 'text-red-600'"
-                                >
-                                    {{ item.profit.toLocaleString('pt-BR') }} u
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-            </u-card>
-        </div>
-    </div>
+								<p
+									class="font-semibold"
+									:class="item.profit >= 0 ? 'text-purple-600' : 'text-red-600'"
+								>
+									{{ item.profit.toLocaleString('pt-BR') }} u
+								</p>
+							</div>
+						</div>
+					</div>
+				</template>
+			</u-card>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -52,16 +44,24 @@ import { LineChart } from "vue-chart-3";
 
 const props = defineProps({
   modelValue: {
-    type: Boolean,
-    required: true,
-    default: () => true
+	type: Boolean,
+	required: true,
+	default: () => true
+  },
+
+  bankrollData: {
+	type: Object,
+	required: true,
+	default: () => {}
   }
 })
+
+console.log(props.modelValue);
 
 const runtimeConfig = useRuntimeConfig();
 const apiUrl = runtimeConfig.public.API_URL;
 
-if (process.client) {
+if (import.meta.client) {
   const zoomPlugin = (await import("chartjs-plugin-zoom")).default;
   const annotationPlugin = (await import("chartjs-plugin-annotation")).default;
   Chart.register(zoomPlugin);
@@ -73,51 +73,51 @@ const chartOptions = ref({
   responsive: true,
   maintainAspectRatio: false,
   transitions: {
-    zoom: {
-      animation: {
-        duration: 1000,
-        easing: "easeOutCubic",
-      },
-    },
+	zoom: {
+	  animation: {
+		duration: 1000,
+		easing: "easeOutCubic",
+	  },
+	},
   },
   plugins: {
-    legend: {
-      position: "top",
-      display: true,
-    },
-    zoom: {
-      zoom: {
-        wheel: {
-          enabled: true,
-        },
-        pinch: {
-          enabled: true,
-        },
-        mode: "x",
-        drag: {
-          enabled: true,
-          borderColor: "rgb(54, 162, 235)",
-          borderWidth: 1,
-          backgroundColor: "rgba(54, 162, 235, 0.3)",
-        },
-      },
-      pan: {
-        enabled: true,
-        mode: "x",
-        modifierKey: "ctrl",
-      },
-    },
-    annotation: {
-      annotations: {
-        line1: {
-          type: "line",
-          xMin: -100,
-          xMax: -100,
-          borderColor: "rgb(20 184 166)",
-          borderWidth: 2,
-        },
-      },
-    },
+	legend: {
+	  position: "top",
+	  display: true,
+	},
+	zoom: {
+	  zoom: {
+		wheel: {
+		  enabled: true,
+		},
+		pinch: {
+		  enabled: true,
+		},
+		mode: "x",
+		drag: {
+		  enabled: true,
+		  borderColor: "rgb(54, 162, 235)",
+		  borderWidth: 1,
+		  backgroundColor: "rgba(54, 162, 235, 0.3)",
+		},
+	  },
+	  pan: {
+		enabled: true,
+		mode: "x",
+		modifierKey: "ctrl",
+	  },
+	},
+	annotation: {
+	  annotations: {
+		line1: {
+		  type: "line",
+		  xMin: -100,
+		  xMax: -100,
+		  borderColor: "rgb(20 184 166)",
+		  borderWidth: 2,
+		},
+	  },
+	},
   },
 });
 
@@ -126,57 +126,38 @@ const chartStyle = ref({
   width: "100%",
 });
 
-const { data: bankrollData, pending } = await useLazyFetch(`${apiUrl}/bankroll-evolution`,
-{
-    params: {
-        filtered: props.modelValue,
-    }
-});
-
 const chartData = computed(() => {
-    let labels = bankrollData.value.map((item) => item.Month);
-    let data = bankrollData.value.map((item) => item.Bankroll);
+  let labels = props.bankrollData.map((item) => item.Month);
+  let data = props.bankrollData.map((item) => item.Bankroll);
 
-    return {
-        labels: labels,
-        datasets: [
-            {
-                label: "Acúmulo de capital",
-                data: data,
-                borderColor: "#6d28d9",
-                backgroundColor: "rgb(109, 40, 217, 0.05)",
-                pointRadius: 3,
-                pointHoverRadius: 7,
-                fill: true,
-                tension: 0.2,
-            },
-        ],
-    };
+  return {
+	labels: labels,
+	datasets: [
+	  {
+		label: "Acúmulo de capital",
+		data: data,
+		borderColor: "#6d28d9",
+		backgroundColor: "rgb(109, 40, 217, 0.05)",
+		pointRadius: 3,
+		pointHoverRadius: 7,
+		fill: true,
+		tension: 0.2,
+	  },
+	],
+  };
 })
 
 const resultsByMonth = computed(() => {
-    let removedInitialMonth = bankrollData.value.slice(1);
-    let months = removedInitialMonth.map((item) => {
-        return {
-            month: item.Month,
-            profit: item.Profit,
-        }
-    })
+	let removedInitialMonth = props.bankrollData.slice(1);
+	let months = removedInitialMonth.map((item) => {
+		return {
+			month: item.Month,
+			profit: item.Profit,
+		}
+	})
 
-    return months.reverse();
+	return months.reverse();
 })
-
-watchEffect(async () => {
-  pending.value = true;
-
-  bankrollData.value = await $fetch(`${apiUrl}/bankroll-evolution`, {
-    params: {
-      filtered: props.modelValue,
-    }
-  })
-
-  pending.value = false;
-});
 
 </script>
 
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,90 +1,98 @@
 <template>
   <div class="flex flex-col gap-5">
-    <div class="flex justify-between items-end">
-      <page-header title="Bem-vindo ao PrimeTime!" />
-      <div class="flex gap-2">
-        <div class="inline-block align-middle">
-          <UToggle
-            size="lg"
-            on-icon="i-heroicons-check-20-solid"
-            off-icon="i-heroicons-x-mark-20-solid"
-            :loading="pending"
-            :model-value="onlyChosenModels"
-            @click="onlyChosenModels = !onlyChosenModels"
-          />
-        </div>
+	<div class="flex justify-between items-end">
+	  <page-header title="Bem-vindo ao PrimeTime!" />
+	  <div class="flex gap-2">
+		<div class="inline-block align-middle">
+		  <UToggle
+			size="lg"
+			on-icon="i-heroicons-check-20-solid"
+			off-icon="i-heroicons-x-mark-20-solid"
+			:loading="monthDataStatus === 'pending'"
+			:model-value="onlyChosenModels"
+			@click="onlyChosenModels = !onlyChosenModels"
+		  />
+		</div>
 
-        <p>Apenas modelos selecionados</p>
-      </div>
-    </div>
+		<p>Apenas modelos selecionados</p>
+	  </div>
+	</div>
 
-    <u-card>
-      <template #header>
-        <p class="font-semibold">Evolução da banca</p>
-      </template>
+	<u-skeleton
+	  v-if="yesterdayDataStatus === 'pending'"
+	  class="w-full h-[510px]"
+	/>
 
-      <div class="w-full">
-        <bankroll-evolution :model-value="onlyChosenModels" />
-      </div>
-    </u-card>
+	<u-card v-else>
+	  <template #header>
+		<p class="font-semibold">Evolução da banca</p>
+	  </template>
 
-    <u-skeleton
-      v-if="yesterdayDataPending"
-      class="w-full h-[330px]"
-    />
+	  <div class="w-full">
+		<bankroll-evolution
+		  :model-value="status === 'pending'"
+		  :bankroll-data="bankrollData"
+		/>
+	  </div>
+	</u-card>
 
-    <u-card v-else>
-      <template #header>
-        <p class="font-semibold">{{ !yesterdayDataError ? `Resultados de ontem - ${formatDate(yesterday)}` : `Resultados de anteontem - ${formatDate(dayBeforeYesterday)}`}}</p>
-      </template>
+	<u-skeleton
+	  v-if="yesterdayDataStatus === 'pending'"
+	  class="w-full h-[330px]"
+	/>
 
-      <div class="flex gap-5 w-full">
-        <yesterday-metrics-card
-          :items="yesterdayMetrics"
-        />
+	<u-card v-else>
+	  <template #header>
+		<p class="font-semibold">{{ !yesterdayDataError ? `Resultados de ontem - ${formatDate(yesterday)}` : `Resultados de anteontem - ${formatDate(dayBeforeYesterday)}`}}</p>
+	  </template>
 
-        <ranking-models
-          :title="'Top 3 modelos'"
-          :items="top3YesterdayModels"
-          :all-results-data="yesterdayResults"
-        />
+	  <div class="flex gap-5 w-full">
+		<yesterday-metrics-card
+		  :items="yesterdayMetrics"
+		/>
 
-        <yesterday-details-card
-          :number-bets="yesterdayTotal.Num_Bets"
-          :number-models="yesterdayTotal.Method"
-          :positive-models="positiveYesterdayModels"
-        />
-      </div>
-    </u-card>
+		<ranking-models
+		  :title="'Top 3 modelos'"
+		  :items="top3YesterdayModels"
+		  :all-results-data="yesterdayResults"
+		/>
 
-    <u-skeleton
-      v-if="monthDataPending"
-      class="w-full h-[330px]"
-    />
+		<yesterday-details-card
+		  :number-bets="yesterdayTotal.Num_Bets"
+		  :number-models="yesterdayTotal.Method"
+		  :positive-models="positiveYesterdayModels"
+		/>
+	  </div>
+	</u-card>
 
-    <u-card v-else>
-      <template #header>
-        <p class="font-semibold">Resultados do mês</p>
-      </template>
+	<u-skeleton
+	  v-if="monthDataStatus === 'pending'"
+	  class="w-full h-[330px]"
+	/>
 
-      <div class="flex gap-5 w-full">
-        <yesterday-metrics-card
-          :items="monthMetrics"
-        />
+	<u-card v-else>
+	  <template #header>
+		<p class="font-semibold">Resultados do mês</p>
+	  </template>
 
-        <ranking-models
-          :title="'Top 3 modelos'"
-          :items="top3MonthModels"
-          :all-results-data="monthResults"
-        />
+	  <div class="flex gap-5 w-full">
+		<yesterday-metrics-card
+		  :items="monthMetrics"
+		/>
 
-        <yesterday-details-card
-          :number-bets="monthTotal.Num_Bets"
-          :number-models="monthTotal.Method"
-          :positive-models="positiveMonthModels"
-        />
-      </div>
-    </u-card>
+		<ranking-models
+		  :title="'Top 3 modelos'"
+		  :items="top3MonthModels"
+		  :all-results-data="monthResults"
+		/>
+
+		<yesterday-details-card
+		  :number-bets="monthTotal.Num_Bets"
+		  :number-models="monthTotal.Method"
+		  :positive-models="positiveMonthModels"
+		/>
+	  </div>
+	</u-card>
   </div>
 </template>
 
@@ -100,22 +108,28 @@ const yesterday = DateTime.now().minus({ days: 1 }).toFormat('yyyy-MM-dd');
 const dayBeforeYesterday = DateTime.now().minus({ days: 2 }).toFormat('yyyy-MM-dd');
 const onlyChosenModels = ref(true);
 
-const { data: yesterdayData, pending: yesterdayDataPending, error: yesterdayDataError } = await useFetch(`${apiUrl}/daily-results/${yesterday}`, {
+const { data: bankrollData } = await useFetch(`${apiUrl}/bankroll-evolution`,
+{
+	params: {
+		filtered: onlyChosenModels,
+	}
+});
+
+const { data: yesterdayData, status: yesterdayDataStatus, error: yesterdayDataError } = await useFetch(`${apiUrl}/daily-results/${yesterday}`, {
   params: {
-    filtered: onlyChosenModels,
+	filtered: onlyChosenModels,
   },
 });
 
 const { data: dayBeforeYesterdayData } = await useFetch(`${apiUrl}/daily-results/${dayBeforeYesterday}`, {
   params: {
-    filtered: onlyChosenModels,
+	filtered: onlyChosenModels,
   },
 })
 
-const { data: monthData, pending: monthDataPending } = await useFetch(`${apiUrl}/monthly-results/${month}`, {
-  lazy: false,
+const { data: monthData, status: monthDataStatus } = await useFetch(`${apiUrl}/monthly-results/${month}`, {
   params: {
-    filtered: onlyChosenModels,
+	filtered: onlyChosenModels,
   },
 });
 
@@ -123,7 +137,7 @@ const yesterdayResults = computed(() => {
   return yesterdayData?.value ? yesterdayData.value : dayBeforeYesterdayData.value;
 });
 
-const monthResults = computed( () => {
+const monthResults = computed(() => {
   return monthData.value;
 });
 
@@ -137,91 +151,91 @@ const monthTotal = computed(() => {
 
 const yesterdayMetrics = computed(() => {
   return [
-    {
-      name: 'Profit',
-      value: yesterdayTotal.value.Profit,
-      sufix: 'u'
-    },
-    {
-      name: 'Investido',
-      value: yesterdayTotal.value.Responsibility,
-      sufix: 'u'
-    },
-    {
-      name: 'ROI',
-      value: yesterdayTotal.value.ROI,
-      sufix: ''
-    }
+	{
+	  name: 'Profit',
+	  value: yesterdayTotal.value.Profit,
+	  sufix: 'u'
+	},
+	{
+	  name: 'Investido',
+	  value: yesterdayTotal.value.Responsibility,
+	  sufix: 'u'
+	},
+	{
+	  name: 'ROI',
+	  value: yesterdayTotal.value.ROI,
+	  sufix: ''
+	}
   ]
 });
 
 const monthMetrics = computed(() => {
   return [
-    {
-      name: 'Profit',
-      value: monthTotal.value.Profit,
-      sufix: 'u'
-    },
-    {
-      name: 'Investido',
-      value: monthTotal.value.Responsibility,
-      sufix: 'u'
-    },
-    {
-      name: 'ROI',
-      value: monthTotal.value.ROI,
-      sufix: ''
-    }
+	{
+	  name: 'Profit',
+	  value: monthTotal.value.Profit,
+	  sufix: 'u'
+	},
+	{
+	  name: 'Investido',
+	  value: monthTotal.value.Responsibility,
+	  sufix: 'u'
+	},
+	{
+	  name: 'ROI',
+	  value: monthTotal.value.ROI,
+	  sufix: ''
+	}
   ]
 });
 
 const top3YesterdayModels = computed(() => {
   let removedLast = yesterdayResults.value.slice(0, -1);
   let sorted = _filter(removedLast).sort((a, b) => {
-    return b.Profit - a.Profit
+	return b.Profit - a.Profit
   }).slice(0, 3);
 
   return [
-    {
-      id: sorted[0].Method_Id,
-      name: sorted[0].Method,
-      profit: sorted[0].Profit,
-    },
-    {
-      id: sorted[1].Method_Id,
-      name: sorted[1].Method,
-      profit: sorted[1].Profit,
-    },
-    {
-      id: sorted[2].Method_Id,
-      name: sorted[2].Method,
-      profit: sorted[2].Profit,
-    },
+	{
+	  id: sorted[0].Method_Id,
+	  name: sorted[0].Method,
+	  profit: sorted[0].Profit,
+	},
+	{
+	  id: sorted[1].Method_Id,
+	  name: sorted[1].Method,
+	  profit: sorted[1].Profit,
+	},
+	{
+	  id: sorted[2].Method_Id,
+	  name: sorted[2].Method,
+	  profit: sorted[2].Profit,
+	},
   ]
 })
 
 const top3MonthModels = computed(() => {
   let removedLast = monthResults.value.slice(0, -1);
   let sorted = _filter(removedLast).sort((a, b) => {
-    return b.Profit - a.Profit
+	return b.Profit - a.Profit
   }).slice(0, 3);
 
   return [
-    {
-      id: sorted[0].Method_Id,
-      name: sorted[0].Method,
-      profit: sorted[0].Profit,
-    },
-    {
-      id: sorted[1].Method_Id,
-      name: sorted[1].Method,
-      profit: sorted[1].Profit,
-    },
-    {
-      id: sorted[2].Method_Id,
-      name: sorted[2].Method,
-      profit: sorted[2].Profit,
-    },
+	{
+	  id: sorted[0].Method_Id,
+	  name: sorted[0].Method,
+	  profit: sorted[0].Profit,
+	},
+	{
+	  id: sorted[1].Method_Id,
+	  name: sorted[1].Method,
+	  profit: sorted[1].Profit,
+	},
+	{
+	  id: sorted[2].Method_Id,
+	  name: sorted[2].Method,
+	  profit: sorted[2].Profit,
+	},
   ]
 })
 
